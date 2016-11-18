dd5eec11dafb7b93d383c5a6833197aaec8e0443
Steven Jay <stevenjasionowicz@gmail.com>
2016-11-19 09:02:46 +1300

    String and number methods

---
+++ strings-and-number-methods/scripts.js
@@ -0,0 +1,37 @@
+"use strict"
+
+let theString = 'Hello, JavaScript is amazing';
+
+// startsWith();
+// endsWith();
+// includes();
+
+//logs true
+console.log(theString.startsWith('Hello'));
+
+//logs false
+console.log(theString.endsWith('great'));
+
+//logs true
+console.log(theString.includes('JavaScript'));
+
+//logs 204 
+console.log(0Xcc);
+
+//binary
+console.log(0b10101110);
+
+//Octal
+console.log(0o543);
+
+//True
+console.log(Number.isFinite(3));
+
+//False
+console.log(Number.isFinite(Infinity));
+
+//True
+console.log(Number.isNaN(NaN));
+
+//True
+console.log(Number.isInteger(-2));
